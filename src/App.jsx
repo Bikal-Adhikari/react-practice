51c7a5677bdced446bdb91148d994ed459bafb7e
Bikal <bikaladhikari5@gmail.com>
2024-05-02 19:08:15 +1000

    handlesubmit to submit the no of para needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import data from "./data";
 
 const App = () => {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
+
   return (
     <section className="section-center">
       <h4>tired of boring lorem ipsum?</h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">paragraphs:</label>
         <input
           type="number"
@@ -17,6 +25,9 @@ const App = () => {
           value={count}
           onChange={(e) => setCount(e.target.value)}
         />
+        <button className="btn" type="submit">
+          generate
+        </button>
       </form>
     </section>
   );
